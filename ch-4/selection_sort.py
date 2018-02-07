d44f33a7e60defa1dfadc224cb49095676fdcc5e
ymr27 <myam0118@gmail.com>
2018-02-07 17:05:37 +0900

    rewrite selection_sort by not using built-in max function

--- ch-4/selection_sort.py
+++ ch-4/selection_sort.py
@@ -1,6 +1,11 @@
 def selection_sort(a):
     for i in range(len(a)-1, 0, -1):
-        mi = a[0:i+1].index(max(a[0:i+1]))
+        mi = 0
+        maxv = a[0]
+        for j in range(i+1):
+            if (a[j] > maxv):
+                mi = j
+                maxv = a[j]
         a[i], a[mi] = a[mi], a[i]
 
     return a
